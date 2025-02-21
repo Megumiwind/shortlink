ddfa7c0a7c1ccce0b44709fc464f16c971c71d83
Megumiwind <1194290372@qq.com>
2025-02-21 20:07:16 +0800

    features:通过分布式锁完善用户注册，使得短时间内不能有多个用户用同一个用户名注册

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * redis常量类
+ */
+public class RedisCacheConstant {
+
+    public static final String USER_LOCK_REGISTER_KEY = "short_link:lock_user_register:";
+
+}
