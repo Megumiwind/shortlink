623a78812c31ccc9b4af4dfe78d43ae22b38eb97
Megumiwind <1194290372@qq.com>
2025-02-21 15:21:34 +0800

    features:后台管理系统用户敏感信息接口返回脱敏

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.nageoffer.shortlink.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
+import com.nageoffer.shortlink.admin.dto.resp.UserActualRespDTO;
 import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +18,25 @@ public class UserController {
 //    @Autowired
     private final UserService userService;
 
+    /**
+     * 根据用户名查询用户信息，脱敏后的
+     * @param username
+     * @return
+     */
     @GetMapping("/api/shortlink/v1/user/{username}")
     public Result<UserRespDTO> getUserByUserName(@PathVariable String username) {
-//        LambdaQueryWrapper<UserDo> queryWrapper = Wrappers.lambdaQuery(UserDo.class).eq(UserDo::getUsername, username);
-//        UserDo one = userService.getOne(queryWrapper);
-//        UserRespDTO userRespDTO = new UserRespDTO();
-//        BeanUtils.copyProperties(one, userRespDTO);
-//        return userRespDTO;
         UserRespDTO result = userService.getUserByUsername(username);
-
         return Results.success(result);
+    }
 
+    /**
+     * 根据用户名查询用户信息，脱敏后的
+     * @param username
+     * @return
+     */
+    @GetMapping("/api/shortlink/v1/actual/user/{username}")
+    public Result<UserActualRespDTO> getActualUserByUserName(@PathVariable String username) {
+
+        return Results.success(BeanUtil.toBean(userService.getUserByUsername(username), UserActualRespDTO.class));
     }
 }
