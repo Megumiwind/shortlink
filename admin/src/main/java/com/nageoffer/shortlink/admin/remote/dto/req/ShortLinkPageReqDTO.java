f6ff98f6480858226b1346bee6c3b7557c144d35
Megumiwind <1194290372@qq.com>
2025-02-23 19:34:04 +0800

    features:新增短链接后管请求调用project中分页查询短链接功能

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,23 @@
+package com.nageoffer.shortlink.admin.remote.dto.req;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.nageoffer.shortlink.admin.dao.entity.ShortLinkDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 短链接分页请求对象
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
