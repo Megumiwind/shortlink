abe415c83340a7186b56e37d7f6cb3db48c0c32e
Megumiwind <1194290372@qq.com>
2025-02-26 20:46:16 +0800

    feature: 开发短链接监控之今日统计

--- project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/entity/LinkStatsTodayDO.java
@@ -2,16 +2,21 @@ package com.nageoffer.shortlink.project.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.nageoffer.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 /**
  * 短链接今日统计实体
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
 @TableName("t_link_stats_today")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LinkStatsTodayDO extends BaseDO {
 
     /**
@@ -47,5 +52,5 @@ public class LinkStatsTodayDO extends BaseDO {
     /**
      * 今日ip数
      */
-    private Integer todayIpCount;
+    private Integer todayUip;
 }
